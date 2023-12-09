1c9983e3d3d2b6df4d7504c618400377c990f3ca
LucStr <luca@mail.com>
2023-12-09 09:53:30 +0100

    Day 9

---
+++ days/9/part1.js
@@ -0,0 +1,27 @@
+let data = require('../../get_data')(9);
+
+const sequences = data.split('\n').map(line =>{
+    const numbers = line.split(' ').map(Number);
+    let tail = [numbers];
+
+    while(!tail[tail.length - 1].every(e => e == 0)){
+        const numbers = tail[tail.length - 1];
+        const differences = [];
+        for(let i = 0; i < numbers.length - 1; i++){
+            differences.push(numbers[i + 1] - numbers[i]);
+        }
+
+        tail.push(differences);
+    };
+
+    let current = 0;
+    for(let i = tail.length - 2; i >= 0; i--){
+        current = tail[i][tail[i].length - 1] + current;
+    }
+
+    return current;
+})
+
+const result = sequences.reduce((a, b) => a + b)
+
+console.log(result);
